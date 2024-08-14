20b15a5f1a53c7f773e4ae162b2440bcce6b43c6
flyingculprit <suryan2021ai@gmail.com>
2024-08-14 09:57:15 +0530

    practice

--- Practise/condition.java
+++ Practise/condition.java
@@ -1,12 +1,20 @@
 import java.util.*;
-public class condi {
+public class condition {
 public static void main(String args[]){
     System.out.println("Input kudu \n AI - 10, IT - 20, CSE - ");
     Scanner vaangu = new Scanner (System.in);  
     System.out.print("dept = ");
     int a = vaangu.nextInt(); 
-    System.out.print("dept = ");
-    
+    if(a==10){
+        System.out.print("You are AI dept");
+    }
+    else if (a==20) {
+        System.out.print("You are IT dept");
+        
+    }
+    else{
+        System.out.println("You are CSE");
+    }
     }
 
 }
