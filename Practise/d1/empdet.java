403521c47ae473a590037af29e06b34321603614
flyingculprit <suryan2021ai@gmail.com>
2024-08-19 17:15:14 +0530

    strings

---
+++ Practise/d1/empdet.java
@@ -0,0 +1,14 @@
+package d1;
+
+public class empdet {
+    public static void main(String args[]){
+        String s = "Vanakam da Maapula";
+        int length = s.length();
+        System.out.println(length);
+        System.out.println(s.charAt(5));
+        System.out.println(s.startsWith("V"));
+        System.out.println(s.toUpperCase());
+        char [] charArray();
+    }
+    
+}
---
+++ Practise/d1/hextooct.java
@@ -0,0 +1,13 @@
+package d1;
+
+import java.util.Scanner;
+
+public class hextooct {
+    public static void main(String[] args) {
+        Scanner vaangu = new Scanner(System.in);
+        int x = vaangu.nextInt();
+        System.out.println("Enter the input");
+        
+    }
+    
+}
