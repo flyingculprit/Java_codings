ce3edc25d4ee384b9d47ad738d5f93520838f93f
flyingculprit <suryan2021ai@gmail.com>
2024-08-30 17:01:32 +0530

    data-struct

---
+++ Training/link.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class link {
+    public static void main(String args[]){
+        Scanner vaangu = new Scanner(System.in);
+        int x = vaangu.nextInt();
+        int count = 0, i;
+        for(i = 0;i<x;i++);
+        {
+            count+=i;
+        }
+        System.out.println(count);
+    }
+}
